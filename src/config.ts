0b8b57cbac42c7fcdb9e708ec9980697823afa03
Sylphy-0xd3ac <Sylphy@240729.xyz>
2025-03-29 13:41:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,20 +4,20 @@
  * MongoDB 数据库连接字符串
  * 例如：'mongodb://localhost:27017/npmdb'
  */
-export const MONGO_URI = 'mongodb://localhost:27017/glo-pm-backend';
+export const MONGO_URI = process.env.MONGO_URI;
 
 /**
  * 使用的数据库名称
  */
-export const DB_NAME = 'glo-pm-backend';
+export const DB_NAME = process.env.DB_NAME;
 
 /**
  * 服务监听的端口
  */
-export const PORT = 3000;
+export const PORT = process.env.PORT;
 
 /**
  * 账户密码(如有)
  */
-export const ACCOUNT = null;
-export const PASSWORD = null;
+export const ACCOUNT = process.env.ACCOUNT ? process.env.ACCOUNT : null;
+export const PASSWORD = process.env.PASSWORD ? process.env.PASSWORD : null;
